1df38337214b64cef8e0604414e20c439192291e
Md. Shahriar Haque <81591350+xenoncolt@users.noreply.github.com>
2023-11-27 20:04:36 +0600

    add: open interactively instead of opening a browser (forget to upload)

---
+++ app/phone/src/main/java/dev/jdtech/jellyfin/fragments/WebViewFragment.kt
@@ -0,0 +1,32 @@
+package dev.jdtech.jellyfin.fragments
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import dev.jdtech.jellyfin.R
+
+class WebViewFragment : Fragment() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_web_view, container, false)
+        webView = view.findViewById(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.loadWithOverviewMode = true // Auto adjust the content to fit the screen
+        webView.settings.useWideViewPort = true // Enable normal viewport
+        webView.webViewClient = WebViewClient()
+        val url = arguments?.getString("url")
+        webView.loadUrl(url ?: "")
+        return view
+    }
+}
