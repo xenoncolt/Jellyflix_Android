20f1f4005d1bc3df573989f85193071c586f6423
Md. Shahriar Haque <81591350+xenoncolt@users.noreply.github.com>
2023-11-29 02:17:36 +0600

    rm: removed navigation bar

--- app/phone/src/main/java/dev/jdtech/jellyfin/MainActivity.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/MainActivity.kt
@@ -95,13 +95,13 @@ class MainActivity : AppCompatActivity() {
                 R.id.twoPaneSettingsFragment, R.id.serverSelectFragment, R.id.addServerFragment, R.id.loginFragment, com.mikepenz.aboutlibraries.R.id.about_libraries_dest, R.id.usersFragment, R.id.serverAddressesFragment, R.id.WebViewFragment -> View.GONE
                 else -> View.VISIBLE
             }
-//            if (destination.id == R.id.WebViewFragment) {
-////                supportActionBar?.title = "" // Remove title
-////                supportActionBar?.setDisplayHomeAsUpEnabled(false) // Remove back icon
-//                supportActionBar?.hide() // hide the entire bar
-//            } else {
-//                supportActionBar?.show() // show the toolbars for others
-//            }
+            if (destination.id == R.id.WebViewFragment) {
+//                supportActionBar?.title = "" // Remove title
+//                supportActionBar?.setDisplayHomeAsUpEnabled(false) // Remove back icon
+                supportActionBar?.hide() // hide the entire bar
+            } else {
+                supportActionBar?.show() // show the toolbars for others
+            }
             if (destination.id == com.mikepenz.aboutlibraries.R.id.about_libraries_dest) {
                 binding.mainToolbar.title =
                     getString(CoreR.string.app_info)
