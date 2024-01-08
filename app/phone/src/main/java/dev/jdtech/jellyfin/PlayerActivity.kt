b65f0155985accd825808329b6d5d75708e3ff0b
Md. Shahriar Haque <81591350+xenoncolt@users.noreply.github.com>
2024-01-08 22:57:15 +0600

    refactor: replace SharedFlows with Channels for sending events

--- app/phone/src/main/java/dev/jdtech/jellyfin/PlayerActivity.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/PlayerActivity.kt
@@ -42,6 +42,7 @@ import dev.jdtech.jellyfin.mpv.TrackType
 import dev.jdtech.jellyfin.utils.PlayerGestureHelper
 import dev.jdtech.jellyfin.utils.PreviewScrubListener
 import dev.jdtech.jellyfin.viewmodels.PlayerActivityViewModel
+import dev.jdtech.jellyfin.viewmodels.PlayerEvents
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.lang.IllegalArgumentException
@@ -170,8 +171,10 @@ class PlayerActivity : BasePlayerActivity() {
                 }
 
                 launch {
-                    viewModel.navigateBack.collect {
-                        if (it) finish()
+                    viewModel.eventsChannelFlow.collect { event ->
+                        when (event) {
+                            is PlayerEvents.NavigateBack -> finish()
+                        }
                     }
                 }
             }
