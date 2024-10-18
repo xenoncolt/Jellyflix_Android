13548b93cd163359e8e2550f2756479c933914bb
Xenon Colt <s.h.shahriar.haque@gmail.com>
2024-10-19 02:37:15 +0600

    version update

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val appCode = 6
-    const val appName = "0.5.0"
+    const val appCode = 7
+    const val appName = "0.5.1"
 
     const val compileSdk = 34
     const val buildTools = "34.0.0"
